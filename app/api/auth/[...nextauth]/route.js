58e6afa9627349f1f48cfbb1f083353b522e54fb
Prakash Soni <prakashsoni0024@gmail.com>
2025-09-30 23:56:28 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,42 +1,23 @@
-import NextAuth from 'next-auth'
-// import AppleProvider from 'next-auth/providers/apple'
-// import FacebookProvider from 'next-auth/providers/facebook'
-// import GoogleProvider from 'next-auth/providers/google'
-// import EmailProvider from 'next-auth/providers/email'
-import GitHubProvider from "next-auth/providers/github";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 import mongoose from "mongoose";
 import connectDb from '@/db/connectDb';
-import User from '@/models/User';
-import Payment from '@/models/Payment';
- 
+import User from "@/models/User";
+import Payment from "@/models/Payment";
 
-export const authoptions =  NextAuth({
-    providers: [
-      // OAuth authentication providers...
-      GitHubProvider({
-        clientId: process.env.GITHUB_ID,
-        clientSecret: process.env.GITHUB_SECRET
-      }),
-    //   AppleProvider({
-    //     clientId: process.env.APPLE_ID,
-    //     clientSecret: process.env.APPLE_SECRET
-    //   }),
-    //   FacebookProvider({
-    //     clientId: process.env.FACEBOOK_ID,
-    //     clientSecret: process.env.FACEBOOK_SECRET
-    //   }),
-    //   GoogleProvider({
-    //     clientId: process.env.GOOGLE_ID,
-    //     clientSecret: process.env.GOOGLE_SECRET
-    //   }),
-    //   // Passwordless / email sign in
-    //   EmailProvider({
-    //     server: process.env.MAIL_SERVER,
-    //     from: 'NextAuth.js <no-reply@example.com>'
-    //   }),
-    ],
-    callbacks: {
-      async signIn({ user, account, profile, email, credentials }) {
+export const authOptions = NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+
+    // ...add more providers here
+  ],
+
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
          if(account.provider == "github") { 
           await connectDb()
           // Check if the user already exists in the database
@@ -59,5 +40,5 @@ export const authoptions =  NextAuth({
       },
     } 
   })
+export { authOptions as GET, authOptions as POST };
 
-  export { authoptions as GET, authoptions as POST}
