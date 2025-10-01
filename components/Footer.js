ca2220760968eda437109a983ed4b33a6dc87c59
prakash soni <prakashsoni0024@gmail.com>
2025-10-01 18:36:00 +0530

    change footer size

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer className='bg-gray-900 text-gray-300 flex justify-center items-center px-4 h-16'>
-      <p className='text-[15px]'>Copyright &copy; {currentYear} LaunchPad - All rights reserved.</p>
+      <p className='md:text-[15px]'>Copyright &copy; {currentYear} LaunchPad - All rights reserved.</p>
     </footer>
   );
 }
